0f4296a732be2db0038887c7aeb0ef925257a025
ziens8 <zienek12319wp.pl>
2026-04-09 19:54:27 +0200

    Usuniecie kodu testowego i rozpoczecie pracy

--- APBD-CW06/Program.cs
+++ APBD-CW06/Program.cs
@@ -8,11 +8,13 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+        //dodane
+        builder.Services.AddControllers();
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -23,26 +25,8 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
+        //dodane
+        app.MapControllers();
         app.Run();
     }
 }
